183e289bd7fd65b3ac6d342db54ec1f101117eff
ItoshikiMonset <46911980+ItoshikiMonset@users.noreply.github.com>
2019-11-04 07:51:24 -0500

    Update files.php

--- files.php
+++ files.php
@@ -69,11 +69,11 @@ $default_timezone = 'Etc/UTC'; // UTC
 
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
-$root_path = $_SERVER['Files/'];
+$root_path = $_SERVER[''];
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
-$root_url = '';
+$root_url = 'files';
 
 // Server hostname. Can set manually if wrong
 $http_host = $_SERVER['HTTP_HOST'];
